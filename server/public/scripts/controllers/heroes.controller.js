7320a8865ae12f4584fda80ecc45a6700d44709a
Andrew Froiland <afroiland@Andrews-MacBook-Pro-2.local>
2016-11-28 14:16:40 -0600

    list of heroes displaying in heroes view

--- server/public/scripts/controllers/heroes.controller.js
+++ server/public/scripts/controllers/heroes.controller.js
@@ -1,3 +1,21 @@
 app.controller("HeroesController", ["$http", function($http) {
   console.log('HeroesController running');
+  var self = this;
+
+  self.heroArray = [];
+
+  getHeroes();
+
+  function getHeroes() {
+    console.log('getting heroes');
+    $http.get('/heroes')
+      .then(function(response) {
+        self.heroArray = response.data;
+        console.log('self.heroArray: ', self.heroArray);
+      });
+  }
+
+
+
+
 }]);
